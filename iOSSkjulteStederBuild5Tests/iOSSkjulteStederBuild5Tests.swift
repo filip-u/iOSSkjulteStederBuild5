c2270ab2c08e4d21841fdb622c95f8381d9e257e
byr1al <david.b.hvejsel@gmail.com>
2015-03-10 10:18:05 +0100

    Initial Commit

---
+++ iOSSkjulteStederBuild5Tests/iOSSkjulteStederBuild5Tests.swift
@@ -0,0 +1,36 @@
+//
+//  iOSSkjulteStederBuild5Tests.swift
+//  iOSSkjulteStederBuild5Tests
+//
+//  Created by David Byrial Hvejsel on 10/03/15.
+//  Copyright (c) 2015 DBH. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class iOSSkjulteStederBuild5Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
