9848b56c93a3c6f311d4a579f9072ef77d7be4df
filip-u <FilipUlatowski@Filips-MBP.lan>
2015-03-15 23:54:24 +0100

    fully designed choice screen with reversing animation

---
+++ iOSSkjulteStederBuild5/mainContainer.swift
@@ -0,0 +1,21 @@
+//
+//  mainContainer.swift
+//  iOSSkjulteStederBuild5
+//
+//  Created by Filip Ulatowski on 3/15/15.
+//  Copyright (c) 2015 DBH. All rights reserved.
+//
+
+import UIKit
+
+class mainContainer: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
